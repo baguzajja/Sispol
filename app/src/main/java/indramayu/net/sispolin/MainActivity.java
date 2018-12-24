6c831f33cf815eeca7d4404e6d7ee6bf1a162bfa
Baguz Ariyanto <baguzajjja@gmail.com>
2018-12-24 01:37:37 -0800

    GeoLocation
    GeoLocation

--- app/src/main/java/indramayu/net/sispolin/MainActivity.java
+++ app/src/main/java/indramayu/net/sispolin/MainActivity.java
@@ -1,15 +1,21 @@
 package indramayu.net.sispolin;
 
 import android.annotation.SuppressLint;
+import android.app.Dialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
+import android.nfc.Tag;
 import android.os.Bundle;
 import android.os.Environment;
 import android.provider.Browser;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.text.Html;
+import android.util.Log;
 import android.view.KeyEvent;
+import android.webkit.GeolocationPermissions;
 import android.webkit.ValueCallback;
 import android.webkit.WebResourceError;
 import android.webkit.WebResourceRequest;
@@ -35,13 +41,49 @@ import indramayu.net.sispolin.*;
 public class MainActivity extends AppCompatActivity {
 
     private static final String URL ="https://m.polresindramayu.net/";
-    private WebView webView;
+
 
     private ValueCallback<Uri> mUploadMessage;
     private final static int FILECHOOSER_RESULTCODE = 1;
 
     String UA = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 " + "(KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36";
 
+
+    public class GeoWebViewClient extends WebViewClient{
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            setListeners();
+            return true;
+        }
+    }
+
+
+    public class GeoWebChromeClient extends WebChromeClient {
+        @Override
+        public void onGeolocationPermissionsShowPrompt(final String origin, final GeolocationPermissions.Callback callback) {
+            Log.i(TAG, "onGeolocationPermissionsShowPrompt()");
+
+            final boolean remember = false;
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle("Location");
+            builder.setMessage("Would like to use your Current Location").setCancelable(true).setPositiveButton("Allow", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    callback.invoke(origin, true, remember);
+                }
+            }).setNegativeButton("Dont Allow ", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    callback.invoke(origin, false, remember);
+                }
+            });
+            AlertDialog alert = builder.create();
+            alert.show();
+        }
+    }
+
+
+    WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -61,7 +103,13 @@ public class MainActivity extends AppCompatActivity {
         webSettings.setUserAgentString(UA);
         webSettings.setJavaScriptEnabled(true);
         webSettings.setBuiltInZoomControls(false);
-        webView.getSettings().setAllowFileAccess(true);
+        webSettings.setGeolocationEnabled(true);
+        webView.setWebChromeClient(new GeoWebChromeClient());
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setGeolocationDatabasePath(getFilesDir().getPath() );
+
     }
 
     public void setListeners (){
@@ -111,6 +159,7 @@ public class MainActivity extends AppCompatActivity {
         final MyJavaScriptInterface myJavaScriptInterface = new MyJavaScriptInterface(this);
         webView.addJavascriptInterface(myJavaScriptInterface, "AndroidFunction");
     }
+
 
     public class MyJavaScriptInterface {
         Context mContext;
