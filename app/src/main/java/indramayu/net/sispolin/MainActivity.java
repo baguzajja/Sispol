9ee1106387d42401df337e89420af071c5f71c0a
Baguz Ariyanto <baguzajjja@gmail.com>
2018-12-24 00:42:46 -0800

    Upload File
    Upload File

--- app/src/main/java/indramayu/net/sispolin/MainActivity.java
+++ app/src/main/java/indramayu/net/sispolin/MainActivity.java
@@ -1,14 +1,35 @@
 package indramayu.net.sispolin;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.Browser;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.webkit.ValueCallback;
+import android.webkit.WebResourceError;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebChromeClient;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+import indramayu.net.sispolin.R;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import indramayu.net.sispolin.*;
+
+
 
 
 public class MainActivity extends AppCompatActivity {
@@ -16,17 +37,127 @@ public class MainActivity extends AppCompatActivity {
     private static final String URL ="https://m.polresindramayu.net/";
     private WebView webView;
 
+    private ValueCallback<Uri> mUploadMessage;
+    private final static int FILECHOOSER_RESULTCODE = 1;
+
+    String UA = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 " + "(KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        initFields();
+        setListeners();
+
+
+    }
+
+    public void initFields(){
         webView = (WebView) findViewById(R.id.webView);
-        webView.setWebViewClient(new WebViewClient());
         WebSettings webSettings = webView.getSettings();
+        webSettings.setUserAgentString(UA);
         webSettings.setJavaScriptEnabled(true);
         webSettings.setBuiltInZoomControls(false);
-        webView.loadUrl(URL);
+        webView.getSettings().setAllowFileAccess(true);
     }
+
+    public void setListeners (){
+
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
+                webView.loadUrl("about:blank");
+                view.clearHistory();
+            }
+        });
+        webView.setWebChromeClient(new WebChromeClient(){
+            public void onProgressChanged(WebView view, int progress){
+
+            }
+            public void openFileChooser(ValueCallback<Uri> uploadMsg){
+                mUploadMessage =uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("image/*");
+                startActivityForResult(Intent.createChooser(i,"File Chooser"), FILECHOOSER_RESULTCODE);
+            }
+
+            // For Android 3.0+
+            public void openFileChooser(ValueCallback uploadMsg, String acceptType) {
+                mUploadMessage = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("*/*");
+                startActivityForResult(
+                        Intent.createChooser(i, "File Browser"),
+                        FILECHOOSER_RESULTCODE);
+            }
+
+            //For Android 4.1
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+                mUploadMessage = uploadMsg;
+                Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+                i.addCategory(Intent.CATEGORY_OPENABLE);
+                i.setType("image/*");
+                startActivityForResult(Intent.createChooser(i, "File Chooser"), FILECHOOSER_RESULTCODE);
+
+            }
+        });
+
+        webView.loadUrl(URL);
+        final MyJavaScriptInterface myJavaScriptInterface = new MyJavaScriptInterface(this);
+        webView.addJavascriptInterface(myJavaScriptInterface, "AndroidFunction");
+    }
+
+    public class MyJavaScriptInterface {
+        Context mContext;
+
+        MyJavaScriptInterface(Context c) {
+            mContext = c;
+        }
+
+        @JavascriptInterface
+        public void showToast(String toast) {
+            Toast.makeText(mContext, toast, Toast.LENGTH_SHORT).show();
+            // webView.loadUrl("javascript:document.getElementById(\"Button3\").innerHTML = \"bye\";");
+        }
+
+        @JavascriptInterface
+        public void openAndroidDialog() {
+            AlertDialog.Builder myDialog
+                    = new AlertDialog.Builder(MainActivity.this);
+            myDialog.setTitle("DANGER!");
+            myDialog.setMessage("You can do what you want!");
+            myDialog.setPositiveButton("ON", null);
+            myDialog.show();
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    Intent intent) {
+        if (requestCode == FILECHOOSER_RESULTCODE) {
+            if (null == mUploadMessage) return;
+            Uri result = intent == null || resultCode != RESULT_OK ? null
+                    : intent.getData();
+            mUploadMessage.onReceiveValue(result);
+            mUploadMessage = null;
+        }
+    }
+
+
+    private File createImageFile() throws IOException {
+
+        @SuppressLint("SimpleDateFormat") String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "img_" + timeStamp + "_";
+        File storageDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
+        return File.createTempFile(imageFileName, ".jpg", storageDir);
+    }
+
 
     @Override
     public boolean onKeyDown(int keyCode, KeyEvent event) {
